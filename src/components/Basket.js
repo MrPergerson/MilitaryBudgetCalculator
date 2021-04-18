8c33c38bd50855f760bc5d1d3c5488aa2b679afe
Cole Pergerson <cole.pergerson@gmail.com>
2021-04-18 16:48:38 -0700

    deployed react app to github

---
+++ src/components/Basket.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Basket(props) {
+    return <aside className="block col-1">
+        <h2>Cart Item</h2>
+    </aside>
+}
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main(props) {
+    return <main className="block col-2">
+        <h2>Military Products</h2>
+        <div className="row">
+
+        </div>
+    </main>
+}
